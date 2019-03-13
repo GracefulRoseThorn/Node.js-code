7ae70678b0a1fb34d3d21126720d1994b8439417
GracefulRoseThorn <46552746+GracefulRoseThorn@users.noreply.github.com>
2019-03-12 17:37:14 -0700

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ routes.use('/models', models);
 routes.use('/cars', cars);
 
 routes.get('/', (req, res) => {
-  res.status(200).json( message: To Work website: input /cars to view all avalible cars, put /models to view all models. After putting in either cars or models, input id for a specific type cars(1-6), models(1-3) );
+  res.status(200).json( {message: 'To Work website: input /cars to view all avalible cars, put /models to view all models. After putting in either cars or models, input id for a specific type cars(1-6), models(1-3)'} );
 });
 
 module.exports = routes;
